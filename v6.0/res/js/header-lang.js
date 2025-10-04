ad0bb5fab92a5c7b99a4ebffe24642fd0f8cc6f4
Qubit Security Inc. <qubit3d@gmail.com>
2025-10-04 12:09:18 +0900

    Update header-lang.js

--- v6.0/res/js/header-lang.js
+++ v6.0/res/js/header-lang.js
@@ -24,7 +24,7 @@
     document.cookie = cookie;
 
     // 선택 즉시 이동
-    location.href = 'http://v6.plura.io/' + target;
+    location.href = 'https://v6.plura.io/' + target;
   };
 
   // 선택사항: 헤더 버튼 라벨을 쿠키 기준으로 갱신
@@ -36,5 +36,6 @@
   }
   document.addEventListener('DOMContentLoaded', updateLangButton);
 })();
+
 
 
